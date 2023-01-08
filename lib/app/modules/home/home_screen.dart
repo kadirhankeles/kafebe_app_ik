221477633f2c280eaeb1f8d74c6aa0201055e295
Kadirhan Keleş <kadirhankeles@gmail.com>
2023-01-08 14:54:23 +0300

    homescreen finished

--- lib/app/modules/home/home_screen.dart
+++ lib/app/modules/home/home_screen.dart
@@ -8,20 +8,27 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xffcfd8dc),
       appBar: AppBar(
         backgroundColor: Color(0xffe53935),
         toolbarHeight: 10.h,
-        leadingWidth: 15.h,
+        leadingWidth: 33.w,
         leading: Row(
           children: [
             SizedBox(
-              width: 1.h,
+              width: 4.w,
             ),
-            Container(
-              height: 8.h,
-              width: 8.h,
-              decoration: AppConstant.homeProfilePhoto,
-            ),
+            ClipRRect(
+                borderRadius: BorderRadius.circular(50),
+                child: Container(
+                    height: 8.h,
+                    width: 8.h,
+                    decoration: BoxDecoration(),
+                    child: Image(
+                      image: NetworkImage(
+                          "https://www.wanhaber.com/images/haberler/2020/04/vanli-is-adami-erdogan-in-cagrisina-5-milyon-tl-ile-destek-verdi_b5483.jpg"),
+                      fit: BoxFit.cover,
+                    ))),
             IconButton(
                 splashRadius: 2.h,
                 onPressed: () {},
@@ -30,33 +37,170 @@ class HomeScreen extends StatelessWidget {
         ),
         title: Row(
           children: [
-            
-                SizedBox(
-                  width: 4.h,
-                ),
+            SizedBox(
+              width: 6.w,
+            ),
             Container(
               height: 10.h,
               width: 9.h,
-              color: Colors.white,
-              child: Center(child: Text("Logo", style: TextStyle(color: Colors.black),),),
+              //color: Colors.white,
+              child: Image(
+                image: AssetImage("assets/logo.png"),
+                fit: BoxFit.cover,
+              ),
             ),
-            
-            
           ],
-          
         ),
         actions: [
-          IconButton(onPressed: () {
-              
-            }, icon: Icon(Icons.notifications_active), color: Colors.yellow),
+          IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.notifications_active),
+              color: Colors.yellow),
         ],
       ),
-      body: Column(
-        children: [
-          Container(
-            child: Text("Home Screen"),
-          )
-        ],
+      body: Padding(
+        padding: EdgeInsets.all(2.h),
+        child: Column(
+          children: [
+            // Gridview'ın bulunduğu container yapısı
+            Container(
+              width: 100.w,
+              child: GridView.builder(
+                shrinkWrap: true,
+                itemCount: 4,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 2.h,
+                    mainAxisSpacing: 2.h,
+                    childAspectRatio: 0.15.h),
+                itemBuilder: (context, index) {
+                  return GestureDetector(
+                    onTap: () {
+                      print("Tıklandı $index");
+                    },
+                    child: Container(
+                      decoration: AppConstant.homeButton,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          AppConstant().homeCircularPhoto(
+                              "https://iconarchive.com/download/i103201/blackvariant/button-ui-nik-collection/HDR-Efex-Pro.ico"),
+                          SizedBox(
+                            height: 1.5.h,
+                          ),
+                          Text(
+                            "TEXT",
+                            style: TextStyle(
+                                color: Colors.white, fontWeight: FontWeight.bold),
+                          )
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+            SizedBox(height: 2.h,),
+            //Bordrolarım butonunun tasarımı
+            GestureDetector(
+              onTap: () {
+                print("Tıklandı Bordrolarım");
+              },
+              child: Container(
+                height: 10.h,
+                width: double.infinity,
+                decoration: AppConstant.homeButton,
+                child: Row(
+                  children: [
+                    SizedBox(
+                      width: 5.w,
+                    ),
+                    AppConstant().homeCircularPhoto(
+                        "https://iconarchive.com/download/i103201/blackvariant/button-ui-nik-collection/HDR-Efex-Pro.ico"),
+                    SizedBox(
+                      width: 3.w,
+                    ),
+                    Text("Bordrolarım",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18))
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 2.h,),
+            // İZİNLERİM BUTON TASARIMI
+            GestureDetector(
+              onTap: () {
+                print("Tıklandı izinlerim");
+              },
+              child: Container(
+                height: 15.5.h,
+                width: double.infinity,
+                decoration: AppConstant.homeButton,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      children: [
+                        SizedBox(
+                          width: 5.w,
+                        ),
+                        AppConstant().homeCircularPhoto(
+                            "https://iconarchive.com/download/i103201/blackvariant/button-ui-nik-collection/HDR-Efex-Pro.ico"),
+                        SizedBox(
+                          width: 3.w,
+                        ),
+                        Text("İzinlerim",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18)),
+                        SizedBox(width: 33.w,),
+                        IconButton(
+                          splashColor: Colors.white,
+                          splashRadius: 1.h,
+                          onPressed: () {
+                          print("Tıklandı artı");
+                        }, icon: Icon(Icons.add, color: Colors.white, size: 32,))
+                      ],
+                    ),
+                    SizedBox(height: 1.h,),
+                    Row(
+                      children: [
+                        SizedBox(width: 5.w,),
+                        Column(crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("İzin Bakiyesi", style: AppConstant.homeIzinlerimAciklama,),
+                            SizedBox(height: 0.5.h,),
+                            Text("data",style: AppConstant.homeIzinlerimAciklama,),
+                          ],
+                        ),
+                        SizedBox(width: 3.w,),
+                        Column(crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("İzin Hakediş Tarihi", style: AppConstant.homeIzinlerimAciklama,),
+                            SizedBox(height: 0.5.h,),
+                            Text("data",style: AppConstant.homeIzinlerimAciklama,),
+                          ],
+                        ),
+                        SizedBox(width: 3.w,),
+                        Column(crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("İzin Hakediş Gün Sayısı", style: AppConstant.homeIzinlerimAciklama,),
+                            SizedBox(height: 0.5.h,),
+                            Text("data", style: AppConstant.homeIzinlerimAciklama,),
+                          ],
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/app/utils/app_constant.dart
+++ lib/app/utils/app_constant.dart
@@ -2,6 +2,7 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
 
 class AppConstant{
   //Color
@@ -15,4 +16,25 @@ class AppConstant{
   //padding 
 
   static BoxDecoration homeProfilePhoto = BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(50));
+  static BoxDecoration homeButton = BoxDecoration(color: Color(0xffe53935), borderRadius: BorderRadius.circular(10),boxShadow: [
+      BoxShadow(
+        color: Colors.white.withOpacity(.2),
+        spreadRadius: 2,
+        blurRadius: 6,
+        offset: Offset(3, 6), // changes position of shadow
+      ),
+    ], );
+  static TextStyle homeIzinlerimAciklama = TextStyle(color: Colors.white, fontSize: 13);
+  ClipRRect homeCircularPhoto(String path){
+    return ClipRRect(
+                            borderRadius: BorderRadius.circular(50),
+                            child: Container(
+                                height: 8.h,
+                                width: 8.h,
+                                child: Image(
+                                  image: NetworkImage(
+                                      "${path}"),
+                                  fit: BoxFit.cover,
+                                )));
+  }
 }
