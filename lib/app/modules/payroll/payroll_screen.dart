2229e9eb183f751f93e1688d18ece5362ae3e8d5
Kadirhan Keleş <kadirhankeles@gmail.com>
2023-01-09 20:55:00 +0300

    payrollscreen design changed

--- lib/app/modules/payroll/payroll_screen.dart
+++ lib/app/modules/payroll/payroll_screen.dart
@@ -11,25 +11,39 @@ class PayrollScreen extends GetView<PayrollController> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xffcfd8dc),
-      body: Column(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+             backgroundColor: Color(0xff850000),
+             toolbarHeight: 7.h,
+             elevation: 0,
+             shape: ContinuousRectangleBorder(
+              borderRadius: BorderRadius.only(bottomLeft: Radius.circular(30), bottomRight: Radius.circular(30))
+             ),
+             title: GestureDetector(
+              onTap: () {
+                print("object");
+              },
+              child: Row(
+                children: [
+                  SizedBox(width: 8.w,),
+                  Text("Son Bordronuz: data", style: AppConstant.payrollLastStyle,),
+                ],
+              ),
+             ),
+          ),
+          SliverToBoxAdapter(
+            child: SingleChildScrollView(
+              child:Column(
       children: [
-      Container(
-        height: 15.h,
-        width: double.infinity,
-        decoration: AppConstant.payrollLastpayroll,
-        child: Center(
-            child: Text(
-          "Son Bordronuz: data",
-          style: AppConstant.payrollLastStyle,
-        )),
-      ),
       
       Padding(
-        padding: EdgeInsets.only(right: 1.h, left: 1.h),
+        padding: EdgeInsets.only(right: 2.5.h, left: 2.5.h),
         child: Container(
-          height: 84.h,
+          height: 100.h,
           width: double.infinity,
           child: ListView.builder(
+            //physics: NeverScrollableScrollPhysics(),
             padding: EdgeInsets.zero,
             itemCount: 20,
             shrinkWrap: true,
@@ -38,11 +52,21 @@ class PayrollScreen extends GetView<PayrollController> {
                   onTap: () {},
                   child: Container(
                       height: 7.h,
+
+                      margin: EdgeInsets.only(bottom: 0.6.h),
                       child: Card(
                         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
                         elevation: 6,
-                        child: Center(
-                          child: Text("data", style: AppConstant.payrollStyle,),
+                        child: Row(mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            SizedBox(width: 6.w,),
+                            Container(
+                              height: 7.h,
+                              width: 70.w,
+                              padding: EdgeInsets.only(top: 2.h),
+                              child: Text("Şubat - 2021", style: AppConstant.payrollStyle,)),
+                            Icon(Icons.arrow_forward_rounded)
+                          ],
                         ),
                         semanticContainer: true,
                         clipBehavior: Clip.antiAliasWithSaveLayer,
@@ -52,7 +76,12 @@ class PayrollScreen extends GetView<PayrollController> {
         ),
       )
       ],
-      ),
+      ) ,
+            ),
+          )
+        ],
+      )
+    
     );
   }
 }
