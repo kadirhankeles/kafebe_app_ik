a8aabf0d1917d5f24a0c4ef2adebb108dca4faff
FehmiDeniz <fehmideniz06@gmail.com>
2023-01-09 14:40:23 +0300

    payroll services added

---
+++ lib/app/data/models/payroll_all_model.dart
@@ -0,0 +1,17 @@
+class PayrollAllDataModel {
+  String? date;
+
+  PayrollAllDataModel({this.date});
+
+  PayrollAllDataModel.fromJson(Map<String, dynamic> json) {
+    date = json['Date'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['Date'] = this.date;
+    return data;
+  }
+}
+
+//step 1 
