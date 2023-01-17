d5e2507dbfec2134d317820c53c12c5e00c68ef5
BerkayKaranfil <berkaykaranfil97@gmail.com>
2023-01-17 11:12:42 +0300

    Academy service and model fixed.

--- lib/app/data/services/academy/academy_service.dart
+++ lib/app/data/services/academy/academy_service.dart
@@ -1,13 +1,27 @@
-import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-void main() async {
+import 'package:http/http.dart' as http;
+import 'package:kafebe_app_ik/app/data/models/learning_global_model.dart';
+
+Future<LearningGlobalModel?> getAcademyService() async {
+  LearningGlobalModel data = LearningGlobalModel();
   var headers = {
     'Accept': 'application/json',
-    'vbtauthorization': 'shwjCj+ErPrhOZikoYEuTiV3bvMzcRE0mbgEXqhSbPtYuRk4U94EemoqET1Lp/Dq~1694~string~638095072172983409',
+    'vbtauthorization':
+        'ZqN33no4KcO7HjUnoFb4qivobG3vwG5SR4WjVmsd/kWKT8kUvQJ84tQHlG72RYNM~1694~string~638095464010792720',
   };
 
-  var url = Uri.parse('https://suniktest.suntekstil.com.tr/mobileapi/api/RequestManagement/GetPendingJobs');
-  var res = await http.post(url, headers: headers);
-  if (res.statusCode != 200) throw Exception('http.post error: statusCode= ${res.statusCode}');
+  var params = {
+    'idHrEmployee': '7212',
+  };
+  var query = params.entries.map((p) => '${p.key}=${p.value}').join('&');
+
+  var url = Uri.parse(
+      'https://suniktest.suntekstil.com.tr/mobileapi/api/LearningGlobalMobile/GetMobileLearnAndImprovementData?$query');
+  var res = await http.get(url, headers: headers);
+  if (res.statusCode != 200)
+    throw Exception('http.get error: statusCode= ${res.statusCode}');
   print(res.body);
+  data = LearningGlobalModel.fromJson(jsonDecode(res.body));
+  return data;
 }
--- lib/app/modules/academy/academy_controller.dart
+++ lib/app/modules/academy/academy_controller.dart
@@ -1,5 +1,23 @@
 import 'package:get/get.dart';
+import 'package:kafebe_app_ik/app/data/models/learning_global_model.dart';
+import 'package:kafebe_app_ik/app/data/services/academy/academy_service.dart';
 
 class AcademyController extends GetxController{
+
+  RxBool isLoading = false.obs;
   
+  LearningGlobalModel? learningGlobalModel;
+
+  @override
+  void onInit() async{
+    await getAcademyData();
+    super.onInit();
+  }
+
+  getAcademyData()async{
+    isLoading.value = false;
+    await getAcademyService();
+    isLoading.value = true;
+  }
+
 }
