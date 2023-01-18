f139d5f06f48ebf1c62cdc9c187b066adf88ce0d
Kadirhan Keleş <kadirhankeles@gmail.com>
2023-01-19 01:19:49 +0300

    work created

---
+++ lib/app/modules/work/work_screen.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:kafebe_app_ik/app/modules/work/work_controller.dart';
+
+class WorkScreen extends GetView<WorkController> {
+  const WorkScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
