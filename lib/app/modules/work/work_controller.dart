b8efac47b8c6ad04c5154f2b5ac912d191dc7ea3
Kadirhan Keleş <kadirhankeles@gmail.com>
2023-01-19 02:51:00 +0300

    work finished

--- lib/app/modules/work/work_controller.dart
+++ lib/app/modules/work/work_controller.dart
@@ -1,13 +1,23 @@
 
 import 'package:get/get.dart';
+import 'package:kafebe_app_ik/app/data/models/get_pending_jobs.dart';
+import 'package:kafebe_app_ik/app/data/services/work/get_pending_jobs_service.dart';
 
 class WorkController extends GetxController {
+  GetPendingJobsModel? getPendingJobsModel;
   RxBool isLoading =false.obs;
+  RxString statu = "Bekliyor".obs;
   @override
   void onInit() {
+    getPendingJobsData("1");
     super.onInit();
   }
 
+  getPendingJobsData(String statu) async{
+    isLoading.value = false;
+    getPendingJobsModel = await getPendingJobsService(statu);
+    isLoading.value=true;
+  }
   
  
  
