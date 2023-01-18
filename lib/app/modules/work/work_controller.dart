ffc9c09aafdf8314f5eb779175087b0da5b7a95f
Kadirhan Keleş <kadirhankeles@gmail.com>
2023-01-19 01:20:02 +0300

    work created

---
+++ lib/app/modules/work/work_controller.dart
@@ -0,0 +1,14 @@
+
+import 'package:get/get.dart';
+
+class WorkController extends GetxController {
+  RxBool isLoading =false.obs;
+  @override
+  void onInit() {
+    super.onInit();
+  }
+
+  
+ 
+ 
+}
