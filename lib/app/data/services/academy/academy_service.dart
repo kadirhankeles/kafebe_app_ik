d616f0d2389d8100f940f00459f90f9464488215
BerkayKaranfil <berkaykaranfil97@gmail.com>
2023-01-18 23:41:59 +0300

    Academy service fixed.

--- lib/app/data/services/academy/academy_service.dart
+++ lib/app/data/services/academy/academy_service.dart
@@ -1,16 +1,24 @@
 import 'dart:convert';
 
+import 'package:get_storage/get_storage.dart';
 import 'package:http/http.dart' as http;
 import 'package:kafebe_app_ik/app/data/models/learning_global_model.dart';
 
 Future<LearningGlobalModel?> getAcademyService() async {
   LearningGlobalModel data = LearningGlobalModel();
-  var headers = {
-    'Accept': 'application/json',
-    'vbtauthorization':
-        'TvYsu26HUXWYomgz6zTmzbAmaeT2OnsqGIjDWOw+CAtWAg7rkYrZ4oMkkz3MyyHP~1694~string~638096538923023534',
-  };
+  String cacheToken=GetStorage().read("token");
 
+
+    Map<String,String> headers = {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'vbtauthorization': cacheToken
+  // var headers = {
+  //   'Accept': 'application/json',
+  //   'vbtauthorization':
+  //       'TvYsu26HUXWYomgz6zTmzbAmaeT2OnsqGIjDWOw+CAtWAg7rkYrZ4oMkkz3MyyHP~1694~string~638096538923023534',
+  // };
+    };
   var params = {
     'idHrEmployee': '6641',
   };
